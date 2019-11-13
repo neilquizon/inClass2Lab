9174d5421413630d7ac83d58576354439c6e9bb0
neilquizon <neil.quizon@gmail.com>
2019-11-13 14:10:02 -0800

    #1 Added Student Class

---
+++ InClass2Lab/InClass2Lab/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass2Lab
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Student(string studentId, string firstName, string lastName)
+        {
+            this.StudentId = studentId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
