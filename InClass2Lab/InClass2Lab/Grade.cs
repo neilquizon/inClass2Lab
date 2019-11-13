062203cab1b09c1f9ec06d07100edf50152e4322
neilquizon <neil.quizon@gmail.com>
2019-11-13 14:54:35 -0800

    #1 Added Grade Class

--- InClass2Lab/InClass2Lab/Grade.cs
+++ InClass2Lab/InClass2Lab/Grade.cs
@@ -2,5 +2,14 @@
 {
     public class Grade
     {
+        public string Student { get; set; }
+
+        public int StudentTotal { get; set; }
+
+        public Grade(string student, int studenttotal)
+        {
+            this.Student = student;
+            this.StudentTotal = studenttotal;
+        }
     }
 }
