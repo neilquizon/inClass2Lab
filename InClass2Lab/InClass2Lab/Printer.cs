a92b250de101b887d5f75bc86d7d263909c14ab3
neilquizon <neil.quizon@gmail.com>
2019-11-13 15:10:03 -0800

    #1 Added Printer Class

---
+++ InClass2Lab/InClass2Lab/Printer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass2Lab
+{
+    class Printer
+    {
+        public static void PrintStudentNames(Section section)
+        {
+            Console.WriteLine("List of students");
+
+            foreach (Student student in section.StudentList)
+            {
+                Console.WriteLine(student.FirstName + " " + student.LastName);
+            }
+            Console.WriteLine();
+        }
+    }
+}
