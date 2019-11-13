fb6d8d73301dcc66269e1f854e9beaa82aab0be7
neilquizon <neil.quizon@gmail.com>
2019-11-13 13:48:58 -0800

    #1 Created Class Section

---
+++ InClass2Lab/InClass2Lab/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass2Lab
+{
+    public class Section
+    {
+        public int SectionNumber { get; set; }
+
+        public string SectionName { get; set; }
+
+        public List<Student> StudentList { get; set; }
+
+        public Section(int sectionNumber, string sectionName, List<Student> studentList)
+        {
+            this.SectionNumber = sectionNumber;
+            this.SectionName = sectionName;
+            this.StudentList = studentList;
+        }
+    }
+}
